8f5ff2a0de6fc96546d19574a57c61b90f2c176d
JesusRamirezGamarra <luciojesusramirezgamarra@gmail.com>
2022-12-05 18:50:57 -0500

    deploy

--- src/app.js
+++ src/app.js
@@ -1,33 +1,34 @@
 import cluster from 'cluster'
 import os from 'os'
 import config from './config/config.js';
-import app from './server/express.server.js';
-import { initDB_Event } from './services/connectDB.js'
+// import app from './server/express.server.js';
+// import { initDB_Event } from './services/connectmongodb.js'
 
-import { Server as HttpServer } from 'http'
-import { Server as IOServer } from 'socket.io'
+
+import Server from "./server/server.js";
+
+// import { Server as HttpServer } from 'http'
+// import { Server as IOServer } from 'socket.io'
+
 
 
 const numCPUs = os.cpus().length;
-if(config.INIT.MODO === 'CLUSTER' && cluster.isPrimary){
+if(config.INIT.IS_CLUSTER && cluster.isPrimary){
+    logger.log('info', `Master ${process.pid} process is running`)
     for (let i = 0; i < numCPUs; i++) {
         cluster.fork()
     }
     cluster.on('exit', (worker) => {
-        console.log(`Worker ${worker.process.pid} died at ${Date()}`);
+        // console.log(`Worker ${worker.process.pid} died at ${Date()}`);
+        logger.log('info', `Worker ${worker.process.pid} died ${new Date().toLocaleString()}`);
         cluster.fork();
     });
 }
 else {
     const init = async () => {
-        initDB_Event()
-        const httpServer = HttpServer(app);
-        const io = new IOServer(httpServer, { cors: { origin: "*" } });
-        global.io = io;
-        const server = httpServer.listen(config.INIT.PORT, () => {
-            console.log(`🎁  ~  Server listening on port  ${server.address().port} - worker process with ${process.pid} started , ready : http://localhost:${server.address().port}/`);
-        });
-        server.on('error', (err) => console.log(`Server Error: ${err}`));
+        // initDB_Event()
+        const server = new Server();
+        server.listen();
     }
     init()
 }
